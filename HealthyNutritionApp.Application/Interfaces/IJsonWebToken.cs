6d2ef7d5c25e0c856db229e0b867bc1459fe69d5
Phạm Duy Hoàng <soyu-se@users.noreply.github.com>
2025-05-22 20:50:30 +0700

    adding jwt services

---
+++ HealthyNutritionApp.Application/Interfaces/IJsonWebToken.cs
@@ -0,0 +1,14 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace HealthyNutritionApp.Application.Interfaces
+{
+    public interface IJsonWebToken
+    {
+        string GenerateAccessToken(IEnumerable<Claim> claims);
+
+        ClaimsPrincipal ValidateToken(string token);
+
+        JwtSecurityToken DecodeToken(string token);
+    }
+}
