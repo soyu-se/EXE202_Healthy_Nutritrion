a458fab7c13dc646e54f24e883fdd8973d090b40
Okasu <88636070+Rivinger7@users.noreply.github.com>
2025-06-07 17:35:08 +0700

    fix cai Hoa noi

--- HealthyNutritionApp.Application/Dto/Product/ProductDto.cs
+++ HealthyNutritionApp.Application/Dto/Product/ProductDto.cs
@@ -10,6 +10,7 @@ namespace HealthyNutritionApp.Application.Dto.Product
         public string Description { get; set; } = string.Empty; // Mô tả sản phẩm
         public double Price { get; set; } // Giá sản phẩm
         public int StockQuantity { get; set; } // Số lượng tồn kho
+        public List<string> ImageUrls { get; set; } = []; // Danh sách URL hình ảnh sản phẩm
         public List<string> CategoryIds { get; set; } // ID danh mục sản phẩm
         public List<string> Tags { get; set; } = []; // Danh sách thẻ sản phẩm
         public NutritionFact NutritionFact { get; set; } // Thông tin dinh dưỡng của sản phẩm
